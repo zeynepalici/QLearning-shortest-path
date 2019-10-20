5023cf7b2af67f44533124bf060dc5e3f3cb66b3
zeynep <aliciizeynep@gmail.com>
2019-10-20 03:25:51 +0300

    great

--- main.c
+++ main.c
@@ -7,13 +7,16 @@ int rewardTable[16][4];
 
 double alpha = 0.2;
 double gama = 0.95;
-double epsilon = 0.25;
+double epsilon = 1;
 
 void printQTable() {
+    printf("\tUP\t\t\tRIGHT\t\tLEFT\t\tDOWN\n");
+
     int i, j;
     for (i = 0; i < 16; i++) {
+        printf("%d.\t", i);
         for (j = 0; j < 4; j++) {
-            printf("%d )   %f ",i, qTable[i][j]);
+            printf("%f\t", qTable[i][j]);
         }
         printf("\n");
     }
@@ -41,10 +44,10 @@ int maxDirection(int state) {
         }
     }
 
-    int ar_direction[4] = {0,0,0,0};
+    int ar_direction[4] = {0, 0, 0, 0};
     int count = 0;
-    for(direction=0; direction<4; direction++){
-        if (max == qTable[state][direction]){
+    for (direction = 0; direction < 4; direction++) {
+        if (max == qTable[state][direction]) {
             ar_direction[direction] = -1;
             count++;
         }
@@ -52,14 +55,14 @@ int maxDirection(int state) {
 
     int ar_random_direction[count];
     int index = 0;
-    for(direction = 0; direction<4; direction++){
-        if(ar_direction[direction] == -1){
+    for (direction = 0; direction < 4; direction++) {
+        if (ar_direction[direction] == -1) {
             ar_random_direction[index] = direction;
             index++;
         }
     }
-   // srand(time(0));
-    int select_index= (rand() % count);
+    // srand(time(0));
+    int select_index = (rand() % count);
     m = ar_random_direction[select_index];
 
 
@@ -67,64 +70,30 @@ int maxDirection(int state) {
 }
 
 int qValueUpdate(int state, int direction) {
-    switch (direction) {
-        case 0://yukari
+    int directionArray[4] = {-4, 1, -1, 4};
 
-            if (state < 4) {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state) -
-                                                                               qTable[state][direction]);
-                return state;
-            } else {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state - 4) -
-                                                                               qTable[state][direction]);
-                return state - 4;
-            }
-
-        case 1: //sag
-            if (state == 3 || state == 7 || state == 11 || state == 15) {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state) -
-                                                                               qTable[state][direction]);
-                return state;
-            } else {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state + 1) -
-                                                                               qTable[state][direction]);
-                return state + 1;
-            }
-
-        case 2: //sol
-            if (state == 0 || state == 4 || state == 8 || state == 12) {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state) -
-                                                                               qTable[state][direction]);
-                return state;
-
-            } else {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state - 1) -
-                                                                               qTable[state][direction]);
-                return state - 1;
-            }
-        case 3: //asagi
-            if (state >= 12) {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-                                                                               + gama * maxQ(state) -
-                                                                               qTable[state][direction]);
-                return state;
-            } else {
-                qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
-
-                                                                               + gama * maxQ(state + 4) -
-                                                                               qTable[state][direction]);
-
-                return state + 4;
-            }
+    int newState = state;
+    int i = 0;
+    for (i = 0; i < 4; i++) {
+        if (i == direction) {
+            newState = state + directionArray[i];
+            break;
+        }
     }
-    printf("***********************");
-    return state;
+
+    if (newState > 15 || newState < 0 || (((state % 4) == 0) && direction == 2) ||
+        (((state % 4) == 3) && direction == 1)) {
+        qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
+                                                                       + gama * maxQ(state) -
+                                                                       qTable[state][direction]);
+        return state;
+    }
+
+    qTable[state][direction] = qTable[state][direction] + alpha * (rewardTable[state][direction]
+                                                                   + gama * maxQ(state + directionArray[direction]) -
+                                                                   qTable[state][direction]);
+    return state + directionArray[direction];
+
 }
 
 double randomDouble(double low, double high) {
@@ -147,32 +116,28 @@ int main() {
     rewardTable[14][1] = 100;
     rewardTable[11][3] = 100;
 
-    int currentstate = rand()%16;
-    //int direction=0;
-
-    int episode = 100000;
+    int currentState = rand() % 16;
+    int episode = 5000;
     while (episode > 0) {
-        printf("current: %d\n", currentstate);
-
+        printf("current: %d\n", currentState);
         double random_value = randomDouble(0.0, 1.0);
         // double random_value = 0.05;
         if (random_value <= epsilon) {
-            int direction = (rand() % 4);
-            currentstate = qValueUpdate(currentstate, direction);
+            int direction = rand() % 4;
+            currentState = qValueUpdate(currentState, direction);
         } else {
-            int direction = maxDirection(currentstate);
-            currentstate = qValueUpdate(currentstate, direction);
-        }
-        if (currentstate == 15) {
-            printf("GOAL!!!! \n");
-            currentstate = rand()%16;
-            //episode--;
-            //printf("count: %d\n", count);
+            int direction = maxDirection(currentState);
+            currentState = qValueUpdate(currentState, direction);
         }
 
+        if (currentState == 15) {
+            printf("current: %d\n", currentState);
+            currentState = rand() % 16;
+            printf("G O A L ! ! !\n\n");
+            episode--;
+        }
 
-        epsilon -= 0.00005;
-        episode--;
+        epsilon -= 1.0 / 5000;
     }
     printQTable();
 
